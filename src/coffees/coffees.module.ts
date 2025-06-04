0963d45fdc3292ab1899ed62edfbcf1435cc940f
Daniyal Ahmed <da9033431@gmail.com>
2025-06-04 12:00:00 +0100

    feat: add Coffees module with Coffee and Flavor entities and their many-to-many relationship.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,10 +3,11 @@ import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
     providers: [CoffeesService],
     controllers: [CoffeesController],
-    imports: [TypeOrmModule.forFeature([Coffee])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
 })
 export class CoffeesModule { }
